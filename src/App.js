f9d46660149ad6f257c23e6981885d1779ce1894
schmlr <98670792+schmlr@users.noreply.github.com>
2022-03-26 13:36:10 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className="Logo" src="https://www.shecodes.io/assets/branding/logo-shecodes-f9fa0540d113c086f61eb6e89466c0cbd24a42163b6a96d4b01da078803f53ee.png" alt="SheCodes Logo"></img>
+      </header>
+      Hello
+    </div>
+  );
+}
